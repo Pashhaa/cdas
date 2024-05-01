581aa67ef2bb33ac089693d81de19ed8d06b194b
pasha <pashock303@gmaill.com>
2024-05-01 13:06:58 +0200

    (feature) adding templates

---
+++ src/main/java/com/example/cdas/config/JWTCore.java
@@ -0,0 +1,9 @@
+package com.example.cdas.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTCore {
+}
---
+++ src/main/java/com/example/cdas/dto/ResponseRequest.java
@@ -0,0 +1,21 @@
+package com.example.cdas.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseRequest {
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private String username;
+    private String email;
+    private String role;
+    private String password;
+}
